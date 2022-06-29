ab815a58c9f17096fb5aa6effe5be417ededb5ce
lmi-1 <lmi-1@rambler.ru>
2022-06-29 23:09:10 +0300

    create def show_field

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 def greet():
     print("-------------------")
     print(" Приветствуем Вас  ")
-    print("     в игре        ")
+    print("      в игре       ")
     print("  крестики-нолики  ")
     print("-------------------")
     print(" формат ввода: x y ")
     print(" x - номер строки  ")
     print(" y - номер столбца ")
+
+def show_field(f):
+    num ='  0 1 2'
+    print(num)
+    for row,i in zip(f,num.split()):
+        print(f"{i} {' '.join(str(j) for j in row)}")
+# field = [['-']*3 for _ in range(3)]
+# show_field(field)
 
