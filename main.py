ee5725b52d4659e53c831044827ff6f1d372dd9c
lmi-1 <lmi-1@rambler.ru>
2022-06-29 23:29:22 +0300

    create def user_input

--- main.py
+++ main.py
@@ -13,6 +13,30 @@ def show_field(f):
     print(num)
     for row,i in zip(f,num.split()):
         print(f"{i} {' '.join(str(j) for j in row)}")
+
+def user_input(f,user):
+    while True:
+        place=input(f"Ходит {user} .Введите координаты: ").split()
+        if len(place)!=2:
+            print('Введите две координаты')
+            continue
+        if not(place[0].isdigit() and place[1].isdigit()):
+            print('Введите числа')
+            continue
+        x, y = map(int, place)
+        if not(x>=0 and x<3 and y>=0 and y<3):
+            print('Вышли из диапазона')
+            continue
+        if f[x][y]!='-':
+            print('Клетка занята')
+            continue
+        break
+    return x,y
+
+
 # field = [['-']*3 for _ in range(3)]
+# greet()
 # show_field(field)
+# user_input(field,'x')
+
 
